64e944279aba679712630d8452c754e7d7e770bc
Danny Hollman <dannyhollman@gmail.com>
2020-04-17 04:36:52 +0000

    [INIT] Add all files

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    # loop through each box (index)
+    for x in range(1, len(boxes) - 1):
+        # Make list of all keys
+        temp = []
+        for index in range(len(boxes)):
+            if index == x:
+                continue
+            for key in boxes[index]:
+                temp.append(key)
+        # check for key
+        if x not in temp:
+            return False
+    return True
