34c41770a83ce4d7f13aea26ff8e7d65f9e07718
Danny Hollman <dannyhollman@gmail.com>
2020-04-26 07:27:22 +0000

    [INIT] Add all files

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node - adds node to sorted linked list
+ * @head: pointer to pointer of first node
+ * @number: value of new node
+ * Return: address of new node or NULL if fail
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+	listint_t *last;
+
+	if (!head)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		if (temp->n >= number)
+		{
+			last->next = new;
+			new->next = temp;
+			new->n = number;
+			break;
+		}
+		last = temp;
+		temp = temp->next;
+	}
+	return (new);
+}
