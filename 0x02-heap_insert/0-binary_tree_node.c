a9b02510b02a25a108c122fb2430dc1c4117d1f9
Danny Hollman <dannyhollman@gmail.com>
2020-05-06 01:02:14 +0000

    [INIT] Add file 0

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+
+	if (parent == NULL)
+		return (new);
+	if (parent->left == NULL)
+	{
+		parent->left = new;
+		new->parent = parent;
+		return (new);
+	}
+	if (parent->right == NULL)
+	{
+		parent->right = new;
+		new->parent = parent;
+		return (new);
+	}
+	return (NULL);
+}
