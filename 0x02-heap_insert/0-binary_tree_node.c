54bf7f7e46b630da7faaf558a1a90ed29ea13c41
Danny Hollman <dannyhollman@gmail.com>
2020-05-06 01:05:45 +0000

    [UPDATE] Add header and fix betty error

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,8 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent node
+ * @value: value of new node
+ * Return: pointer to new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
 	if (new == NULL)
 		return (NULL);
 
@@ -10,17 +17,20 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (new);
+
 	if (parent->left == NULL)
 	{
 		parent->left = new;
 		new->parent = parent;
 		return (new);
 	}
+
 	if (parent->right == NULL)
 	{
 		parent->right = new;
 		new->parent = parent;
 		return (new);
 	}
+
 	return (NULL);
 }
