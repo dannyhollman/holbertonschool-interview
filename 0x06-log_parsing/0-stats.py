4215b756ad47e1187e1b6d5758d1b5c4fdb2af15
Danny Hollman <dannyhollman@gmail.com>
2020-06-02 18:43:59 +0000

    [UPDATE] Remove while loop, change signal handling

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """ parse log and print status codes """
 import sys
+from signal import signal, SIGINT
 
 
 count = 0
 size = 0
 status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-while True:
-    try:
+
+
+def printer():
+    print("File size: " + str(size))
+    for x in sorted(status):
+        if status[x] != 0:
+            print(str(x) + ": " + str(status[x]))
+
+
+def handler(signum, stack):
+    printer()
+    raise
+
+signal(SIGINT, handler)
+
+if __name__ == "__main__":
+    for line in sys.stdin:
         if count == 10:
-            print("File size: " + str(size))
-            for x in sorted(status):
-                if status[x] != 0:
-                    print(str(x) + ": " + str(status[x]))
+            printer()
             count = 0
-        data = sys.stdin.readline()
-        splt = data.split()
+        splt = line.split()
         size += int(splt[8])
         status[int(splt[7])] += 1
         count = count + 1
-    except KeyboardInterrupt:
-        print("File size: " + str(size))
-        for x in sorted(status):
-            if status[x] != 0:
-                print(str(x) + ": " + str(status[x]))
-        sys.exit()
