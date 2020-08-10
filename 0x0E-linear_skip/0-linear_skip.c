f58c60f2ef2e249436a8c5fc2c8ba1d82e6d2847
Danny Hollman <dannyhollman@gmail.com>
2020-08-10 04:59:50 +0000

    [INIT] First push

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,45 @@
+#include "search.h"
+#include <stdio.h>
+
+/**
+ * linear_skip - linear search in a skip list
+ * @list: skip list
+ * @value: value to search for
+ * Return: node with value or null if not found
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *express = list->express;
+	skiplist_t *prev = list;
+	skiplist_t *end;
+
+	while (express != NULL)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+		if (express->n > value)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					prev->index, express->index);
+			express = list;
+			break;
+		}
+		else if (express->express == NULL)
+		{
+			end = express;
+			while (end->next != NULL)
+				end = end->next;
+			printf("Value found between indexes [%lu] and [%lu]\n",
+					express->index, end->index);
+		}
+		prev = express;
+		express = express->express;
+	}
+	while (prev != NULL)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
+	return (NULL);
+}
