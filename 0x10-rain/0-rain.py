1f76aa331660cdd818290840c6dcda2fd0e6cfb4
Danny Hollman <dannyhollman@gmail.com>
2020-08-26 19:29:01 +0000

    [UPDATE] Fix pep8 errors

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+
 def rain(walls):
     total = 0
     n = len(walls)
-    for i in range(1, n - 1): 
+
+    for i in range(1, n - 1):
         left = walls[i]
         for j in range(i):
-            left = max(left, walls[j]) 
-        right = walls[i]; 
-        for j in range(i + 1 , n): 
-            right = max(right, walls[j]);
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
         total = total + (min(left, right) - walls[i])
     return total
