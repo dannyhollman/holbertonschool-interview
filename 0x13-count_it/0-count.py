cc0c9831bdd20e421f67dfa74678f89596f8579c
Danny Hollman <dannyhollman@gmail.com>
2020-09-16 22:35:45 +0000

    [UPDATE] Check all pages

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -9,7 +9,7 @@ def make_dictionary(word_list, dic):
         dic[word] = 0
 
 
-def count_words(subreddit, word_list, dic={}, count=0):
+def count_words(subreddit, word_list, dic={}, after=None):
     """ count words from list in subreddit titles """
     if len(dic) == 0:
         make_dictionary(word_list, dic)
@@ -17,23 +17,20 @@ def count_words(subreddit, word_list, dic={}, count=0):
         u = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
         h = {'User-Agent': 'test'}
         p = {'limit': 100}
-        response = requests.get(u, headers=h, params=p, allow_redirects=False)
-        posts = response.json()['data']['children']
-        if count == len(posts) - 1:
-            printed = 0
-            for k, v in sorted(dic.items()):
-                if v > 0:
-                    print('{}: {}'.format(k, v))
-                    printed += 1
-            if printed == 0:
-                print()
-            return
-        for word in word_list:
-            for split in posts[count]['data']['title'].lower().split(' '):
-                if word == split:
+        if after:
+            p['after'] = after
+        r = requests.get(u, headers=h, params=p, allow_redirects=False).json()
+        posts = r.get('data').get('children')
+        for x in posts:
+            for word in word_list:
+                if word.lower() in x.get('data').get('title').lower().split():
                     dic[word] += 1
-        return count_words(subreddit, word_list, dic, count + 1)
+        after = r.get('data').get('after')
+        if after:
+            return count_words(subreddit, word_list, dic, after)
+        for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True):
+            if v > 0:
+                print('{}: {}'.format(k, v))
 
     except:
-        print()
         return
